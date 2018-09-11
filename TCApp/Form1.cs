ddeb6a57c282f60546473d21dad0ad597b977b4d
absltn <miet.kremerov@gmail.com>
2018-09-11 23:49:22 +0300

    async background threading for heavy operations testing

--- TCApp/Form1.cs
+++ TCApp/Form1.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.IO;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace TCApp
 {
@@ -17,21 +18,113 @@ namespace TCApp
             AppendText(this.richTextBox3, Color.Red, "Deleted text is red.");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void compareAsync(string[] linesOfOrigin, string[] linesToCheck, int[] removed_indexes, List<Colorator> finalList)
+        {
+            for (int i = 0; i < linesOfOrigin.Length; i++)              // initialize array of removed original indexes
+                removed_indexes[i] = 1;
+            for (int i = 0; i < linesToCheck.Length; i++)
+            {
+                bool found = false;
+                for (int j = 0; j < linesOfOrigin.Length; j++)
+                {
+                    if ((linesToCheck[i] == linesOfOrigin[j]) & (!found) && (removed_indexes[j] != 0))
+                    {
+                        Colorator colorator = new Colorator(linesOfOrigin[j], Color.Green);
+                        finalList.Add(colorator);                     // insert colored text into list
+                        removed_indexes[j] = 0;
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    string[] words = linesToCheck[i].Split(new[] { " ", ",", ";", "." },
+                        StringSplitOptions.RemoveEmptyEntries
+                        );
+
+                    for (int j = 0; j < linesOfOrigin.Length; j++)
+                    {
+                        if ((words.Any(linesOfOrigin[j].Contains)) && (removed_indexes[j] != 0))
+                        {
+                            Colorator colorator = new Colorator(linesOfOrigin[j], Color.Red);
+                            Colorator colorator2 = new Colorator(linesToCheck[i], Color.Orange);
+
+                            finalList.Add(colorator);
+                            finalList.Add(colorator2);
+
+                            removed_indexes[j] = 0;
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!found)
+                {
+                    Colorator colorator = new Colorator(linesToCheck[i], Color.Orange);
+                    finalList.Add(colorator);
+                }
+            }
+            
+            for (int i = 0; i < linesOfOrigin.Length; i++)
+            {
+                if (removed_indexes[i] == 1)
+                {
+                    Colorator colorator = new Colorator(linesOfOrigin[i], Color.Red);
+                    finalList.Insert(i + 1, colorator);            // insert colored text into list
+                }
+            }
+        }
+
+        private async System.Threading.Tasks.Task ComputeAsync(string[] origin, string[] changed, int[] rem, List<Colorator> finalList)
+        {
+            compareAsync(origin, changed, rem, finalList);
+        }
+
+        private async void compare(string filename)
+        {
+            richTextBox2.Text = "";                                      // clear richTextBox2 on b2 click
+            string originalText = richTextBox1.Text;
+            string[] linesOfOrigin = originalText.Split(
+            new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.RemoveEmptyEntries
+            );
+
+            string textToCheck = File.ReadAllText(filename);
+            string[] linesToCheck = textToCheck.Split(
+            new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.RemoveEmptyEntries
+            );
+            textBox2.Text = filename;
+            List<Colorator> finalList = new List<Colorator>();
+            int[] removed_indexes = new int[linesOfOrigin.Length];
+
+            await System.Threading.Tasks.Task.Run(() => ComputeAsync(linesOfOrigin, linesToCheck, removed_indexes,finalList));
+            for (int i = 0; i<finalList.Count(); i++)
+                AppendText(richTextBox2, finalList[i].color, finalList[i].text +"\n");
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.ShowDialog();
             string filename = openFileDialog1.FileName;
             string readFile = File.ReadAllText(filename);
             richTextBox1.Text = readFile;
             textBox1.Text = filename;
+            if (richTextBox2.Text != "")
+            {
+                compare(filename);
+            }
         }
-        private void button2_Click(object sender, EventArgs e)
+
+        private async void button2_Click(object sender, EventArgs e)
         {
             if (richTextBox1.Text == "")
             {
                 var form = new Form();
                 var label = new System.Windows.Forms.Label();
-                form.Size = new System.Drawing.Size(200,150);
+                form.Size = new System.Drawing.Size(200, 150);
                 label.Text = "Please select original file first.";
                 label.Size = new System.Drawing.Size(180, 20);
                 form.Controls.Add(label);
@@ -39,88 +132,11 @@ namespace TCApp
             }
             else
             {
-                richTextBox2.Text = "";                                      // clear richTextBox2 on b2 click
-                string originalText = richTextBox1.Text;
-                string[] linesOfOrigin = originalText.Split(
-                    new[] { "\r\n", "\r", "\n" },
-                    StringSplitOptions.RemoveEmptyEntries
-                    );
-                string[] lostLines = new String[linesOfOrigin.Length];
-                for (int i = 0; i < linesOfOrigin.Length; i++)
-                    lostLines[i] = linesOfOrigin[i];
                 openFileDialog1.ShowDialog();
                 string filename = openFileDialog1.FileName;
-                string textToCheck = File.ReadAllText(filename);
-                string[] linesToCheck = textToCheck.Split(
-                    new[] { "\r\n", "\r", "\n" },
-                    StringSplitOptions.RemoveEmptyEntries
-                    );
-                textBox2.Text = filename;
-                List<Colorator> finalList = new List<Colorator>();
-                int indexSize = linesOfOrigin.Length;
-                int[] removed_indexes = new int[indexSize];
-                
-                for (int i = 0; i < indexSize; i++)              // initialize array of removed original indexes
-                    removed_indexes[i] = 1;
-
-                for (int i = 0; i < linesToCheck.Length; i++)
-                {
-                    bool found = false;
-                    for (int j = 0; j < linesOfOrigin.Length; j++)
-                    {
-                        if ((linesToCheck[i] == linesOfOrigin[j])&(!found)&&(removed_indexes[j] != 0))
-                        {
-                            Colorator colorator = new Colorator(linesToCheck[i], Color.Green);
-                            finalList.Add(colorator);                     // insert colored text into list
-                            removed_indexes[j] = 0;
-                            found = true;
-                            break;
-                        }
-                    }
-                
-                    if (!found)
-                    {
-                        string[] words = linesToCheck[i].Split(new[] { " ", ",", ";", "." },
-                            StringSplitOptions.RemoveEmptyEntries
-                            );
-
-                        for (int j = 0; j < linesOfOrigin.Length; j++)
-                        {
-                            if ((words.Any(linesOfOrigin[j].Contains))&&(removed_indexes[j] != 0))
-                            {
-                                Colorator colorator = new Colorator(linesToCheck[i], Color.Orange);
-                                finalList.Add(colorator);          
-
-                                Colorator colorator2 = new Colorator(linesOfOrigin[j], Color.Red);
-                                finalList.Add(colorator2);
-
-                                removed_indexes[j] = 0;
-                                found = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (!found)
-                    {
-                        Colorator colorator = new Colorator(linesToCheck[i], Color.Orange);
-                        finalList.Add(colorator);
-                    }
-                }
-
-                for (int i = 0; i < lostLines.Length; i++)
-                    {
-                        if (removed_indexes[i] == 1)
-                        {
-                            Colorator colorator = new Colorator(lostLines[i], Color.Red);
-                            finalList.Insert(i,colorator);            // insert colored text into list
-                        }
-                    }
-
-                for (int i = 0; i < finalList.Count(); i++)
-                    AppendText(richTextBox2, finalList[i].color,finalList[i].text + "\n");
+                compare(filename);
             }
-
+            
         }      
         void AppendText(RichTextBox box, Color color, string text)
         {
@@ -134,9 +150,18 @@ namespace TCApp
             box.SelectionLength = 0;
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
+    }
+}
 
+class Worker
+{
+    public static void SomeLongOperation(IProgress<string> progress)
+    {
+        // Perform a long running work...
+        for (var i = 0; i < 10; i++)
+        {
+            Task.Delay(500).Wait();
+            progress.Report(i.ToString());
         }
     }
 }
